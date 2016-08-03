47d259e50bf53be5244528bd966196ce3bed96b1
davidaugustmoon <davidaugustmoon@gmail.com>
2016-08-02 20:40:30 -0700

    updated home.php with drop-down email addresses

--- removeVolunteer.php
+++ removeVolunteer.php
@@ -3,7 +3,9 @@
 ini_set('display_errors', 'On');
 //Connects to the database
 //$mysqli = new mysqli("oniddb.cws.oregonstate.edu","ghiraldj-db","v1bptepGowZ4t1OE","ghiraldj-db");
-$mysqli = new mysqli("oniddb.cws.oregonstate.edu","thrashek-db","QybR0dsOjULZ4QtZ","thrashek-db");
+//$mysqli = new mysqli("oniddb.cws.oregonstate.edu","thrashek-db","QybR0dsOjULZ4QtZ","thrashek-db");
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu","moondav-db","BUyiIi84msF2NNtI","moondav-db");
+
 
 if(!$mysqli || $mysqli->connect_errno){
 	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
@@ -19,6 +21,7 @@ if(!$stmt->execute()){
 	echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
 } else {
 	header("Refresh: 0, url=cleanWaterMain.php");
+	//header("Refresh: 0, url=home.php");
 //	echo "Cleared Volunteer ID: " . $_POST["IdNum"] . " from volunteers table.";
 }
 ?>
--- signup.php
+++ signup.php
@@ -34,7 +34,7 @@
   <body>
   	<nav class="navbar navbar-inverse" role="navigation">
   		<div class="navbar-header">
-  			<a href="home.html" class="navbar-brand">Home</a>
+  			<a href="home.php" class="navbar-brand">Home</a>
   		</div>
 
   		<div>
