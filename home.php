b0904f2cb313c93efea33be8028386b1b851e1bb
Jason Ghiraldini <jason.ghiraldini@gmail.com>
2016-08-02 22:28:44 -0700

    Changed java function - testing

--- home.php
+++ home.php
@@ -15,8 +15,12 @@ if($mysqli->connect_errno){
 
 <script>
 function showOccupation() {
-    var lTable = document.getElementById("matches_occupation");
-    lTable.style.display = (lTable.style.display == "table") ? "none" : "table";
+    // var lTable = document.getElementById("matches_occupation");
+    // lTable.style.display = (lTable.style.display == "table") ? "none" : "table";
+    if (document.getElementById("matches_occupation").style.display == "table" ) {
+          document.getElementById("matches_occupation").style.display="none";
+      } else {
+         document.getElementById("matches_occupation").style.display="table";
 }
 </script>
 
