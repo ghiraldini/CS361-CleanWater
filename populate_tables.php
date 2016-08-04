d54a6a16a7cc6c7f29f0dee6bf4d6d391f7e6347
Jason Ghiraldini <jason.ghiraldini@gmail.com>
2016-08-03 21:53:34 -0700

    email typo

--- populate_tables.php
+++ populate_tables.php
@@ -7,7 +7,7 @@ $mysqli = new mysqli("oniddb.cws.oregonstate.edu","ghiraldj-db","v1bptepGowZ4t1O
 if(!($stmt = $mysqli->prepare("SELECT locations.lid, locations.region, locations.country, locations.city, locations.cemail, locations.cphone, locations.opdesc, volunteers.email
 FROM locations
 INNER JOIN volunteers
-WHERE volunteers.email = " . $_POST['emaio']))){
+WHERE volunteers.email = " . $_POST['email']))){
   echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
 }
 if(!$stmt->execute()){
