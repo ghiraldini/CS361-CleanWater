cc83a425216531ed132c598e143108a467700664
Jason Ghiraldini <jason.ghiraldini@gmail.com>
2016-08-03 21:09:13 -0700

    trying to use landing page to pass email

--- home.php
+++ home.php
@@ -124,7 +124,8 @@ if($mysqli->connect_errno){
       OR (volunteers.startDate < locations.endDate AND volunteers.endDate > locations.startDate)
       OR (volunteers.startDate < locations.endDate AND volunteers.endDate < locations.endDate)
       OR (volunteers.startDate > locations.startDate AND volunteers.startDate < volunteers.endDate)
-      ORDER BY locations.lid ASC;"))){
+      AND volunteers.email =" . $_POST["email"]
+      "ORDER BY locations.lid ASC;"))){
         echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
       }
       if(!$stmt->execute()){
